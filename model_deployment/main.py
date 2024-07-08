afe55e9dbd497c7a1fe06083cb9f4eba356585bb
Abdalla Abker <50424683+AbdallaAbker@users.noreply.github.com>
2024-07-08 05:51:24 +0000

    best practices pylint, iisort, black

--- model_deployment/main.py
+++ model_deployment/main.py
@@ -1,8 +1,9 @@
 # main.py
+import pickle
+
+import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
-import pickle
-import numpy as np
 
 # Load the model
 with open("artifacts/model/logreg_model.pkl", "rb") as f:
@@ -10,15 +11,24 @@ with open("artifacts/model/logreg_model.pkl", "rb") as f:
 
 app = FastAPI()
 
+
 # Define the input data model
 class ModelInput(BaseModel):
+    """
+    Define the schema for scoring
+    """
+
     Temperature: float
     Oxygen: float
     Humidity: float
 
+
 # Define the prediction endpoint
 @app.post("/predict")
 def predict(data: ModelInput):
+    """
+    Function to score from an endpoint /prdict
+    """
     # Convert input data to numpy array
     input_data = np.array([[data.Temperature, data.Oxygen, data.Humidity]])
     # Make prediction
@@ -26,6 +36,8 @@ def predict(data: ModelInput):
     # Return the result
     return {"prediction": int(prediction[0])}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
