31ed6a9bb3e787fa5891e1be61e270615d05450d
Abdalla Abker <50424683+AbdallaAbker@users.noreply.github.com>
2024-07-08 06:01:54 +0000

    back .

--- notebook/metrics_calculation.py
+++ notebook/metrics_calculation.py
@@ -1,7 +1,7 @@
 import datetime
 import time
 import random
-import logging 
+import logging
 import uuid
 import pytz
 import pandas as pd
@@ -12,10 +12,16 @@ import joblib
 ############# Evidently Library ###################
 from evidently import ColumnMapping
 from evidently.report import Report
-from evidently.metrics import ColumnDriftMetric, DatasetMissingValuesMetric, DatasetDriftMetric
+from evidently.metrics import (
+    ColumnDriftMetric,
+    DatasetMissingValuesMetric,
+    DatasetDriftMetric,
+)
 
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s"
+)
 
 SEND_TIMEOUT = 10
 rand = random.Random()
@@ -29,75 +35,101 @@ create table dummy_metrics(
 	share_missing_values float
 )
 """
-reference_data = pd.read_csv('../data/raw/reference.csv')
-with open('../artifacts/model/logreg_model.pkl', 'rb') as f_in:
-	model = joblib.load(f_in)
+reference_data = pd.read_csv("../data/raw/reference.csv")
+with open("../artifacts/model/logreg_model.pkl", "rb") as f_in:
+    model = joblib.load(f_in)
 
-raw_data = pd.read_csv('../data/raw/dataset.csv')
+raw_data = pd.read_csv("../data/raw/dataset.csv")
 
 begin = datetime.datetime(2024, 7, 4, 0, 0)
-num_features = ['Temperature', 'Oxygen', 'Humidity']
+num_features = ["Temperature", "Oxygen", "Humidity"]
 cat_features = []
 
 column_mapping = ColumnMapping(
     target=None,
-    prediction= 'Prediction',
-    numerical_features= num_features,
-    categorical_features= cat_features  
+    prediction="Prediction",
+    numerical_features=num_features,
+    categorical_features=cat_features,
 )
 
 
-report = Report(metrics=[ColumnDriftMetric(column_name='Prediction'),
-                         DatasetDriftMetric(),
-                         DatasetMissingValuesMetric()
-                        ]
-                )
+report = Report(
+    metrics=[
+        ColumnDriftMetric(column_name="Prediction"),
+        DatasetDriftMetric(),
+        DatasetMissingValuesMetric(),
+    ]
+)
+
 
 def prep_db():
-	with psycopg.connect("host=localhost port=5432 user=postgres password=example", autocommit=True) as conn:
-		res = conn.execute("SELECT 1 FROM pg_database WHERE datname='test'")
-		if len(res.fetchall()) == 0:
-			conn.execute("create database test;")
-		with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example") as conn:
-			conn.execute(create_table_statement)
-   
+    with psycopg.connect(
+        "host=localhost port=5432 user=postgres password=example",
+        autocommit=True,
+    ) as conn:
+        res = conn.execute("SELECT 1 FROM pg_database WHERE datname='test'")
+        if len(res.fetchall()) == 0:
+            conn.execute("create database test;")
+        with psycopg.connect(
+            "host=localhost port=5432 dbname=test user=postgres password=example"
+        ) as conn:
+            conn.execute(create_table_statement)
+
 
 def calculate_metrics_postgresql(curr, i):
-	current_data = raw_data.iloc[i:i+1]
+    current_data = raw_data.iloc[i : i + 1]
 
-	#current_data.fillna(0, inplace=True)
-	current_data['prediction'] = model.predict(current_data[num_features + cat_features].fillna(0))
+    # current_data.fillna(0, inplace=True)
+    current_data["prediction"] = model.predict(
+        current_data[num_features + cat_features].fillna(0)
+    )
 
-	report.run(reference_data = reference_data, current_data = current_data,
-		column_mapping=column_mapping)
+    report.run(
+        reference_data=reference_data,
+        current_data=current_data,
+        column_mapping=column_mapping,
+    )
 
-	result = report.as_dict()
+    result = report.as_dict()
 
-	prediction_drift = result['metrics'][0]['result']['drift_score']
-	num_drifted_columns = result['metrics'][1]['result']['number_of_drifted_columns']
-	share_missing_values = result['metrics'][2]['result']['current']['share_of_missing_values']
+    prediction_drift = result["metrics"][0]["result"]["drift_score"]
+    num_drifted_columns = result["metrics"][1]["result"][
+        "number_of_drifted_columns"
+    ]
+    share_missing_values = result["metrics"][2]["result"]["current"][
+        "share_of_missing_values"
+    ]
 
-	curr.execute(
-		"insert into dummy_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values) values (%s, %s, %s, %s)",
-		(begin + datetime.timedelta(i), prediction_drift, num_drifted_columns, share_missing_values)
-	)
+    curr.execute(
+        "insert into dummy_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values) values (%s, %s, %s, %s)",
+        (
+            begin + datetime.timedelta(i),
+            prediction_drift,
+            num_drifted_columns,
+            share_missing_values,
+        ),
+    )
 
 
 def batch_monitoring_backfill():
-	prep_db()
-	last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
-	with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example", autocommit=True) as conn:
-		for i in range(0, 27):
-			with conn.cursor() as curr:
-				calculate_metrics_postgresql(curr, i)
+    prep_db()
+    last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
+    with psycopg.connect(
+        "host=localhost port=5432 dbname=test user=postgres password=example",
+        autocommit=True,
+    ) as conn:
+        for i in range(0, 27):
+            with conn.cursor() as curr:
+                calculate_metrics_postgresql(curr, i)
 
-			new_send = datetime.datetime.now()
-			seconds_elapsed = (new_send - last_send).total_seconds()
-			if seconds_elapsed < SEND_TIMEOUT:
-				time.sleep(SEND_TIMEOUT - seconds_elapsed)
-			while last_send < new_send:
-				last_send = last_send + datetime.timedelta(seconds=10)
-			logging.info("data sent")
+            new_send = datetime.datetime.now()
+            seconds_elapsed = (new_send - last_send).total_seconds()
+            if seconds_elapsed < SEND_TIMEOUT:
+                time.sleep(SEND_TIMEOUT - seconds_elapsed)
+            while last_send < new_send:
+                last_send = last_send + datetime.timedelta(seconds=10)
+            logging.info("data sent")
 
-if __name__ == '__main__':
-	batch_monitoring_backfill()
+
+if __name__ == "__main__":
+    batch_monitoring_backfill()
--- test/integration_test.py
+++ test/integration_test.py
@@ -8,19 +8,16 @@ from model_deployment.main import app
 
 client = TestClient(app)
 
+
 def test_prediction():
     url = "/predict"
-    payload = {
-        "Temperature": 25.0,
-        "Oxygen": 21.0,
-        "Humidity": 60.0
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
+    payload = {"Temperature": 25.0, "Oxygen": 21.0, "Humidity": 60.0}
+    headers = {"Content-Type": "application/json"}
 
     response = client.post(url, json=payload, headers=headers)
     assert response.status_code == 200, "Response status code should be 200"
     result = response.json()
     assert "prediction" in result, "Response should contain 'prediction'"
-    assert isinstance(result["prediction"], int), "'prediction' should be an integer"
+    assert isinstance(
+        result["prediction"], int
+    ), "'prediction' should be an integer"
